f0285015c362472bf1b076be241d796a1ce14cf4
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-08-10 06:58:14 +0430

    created index ,about blog page

---
+++ index.php
@@ -0,0 +1,277 @@
+	
+    <?php require_once "inc/head.php"; ?>
+    <?php include_once "inc/navbar.php"; ?>  
+    <?php include_once "admin/auth.php"; ?> 
+	<?php check_access(["admin","user"]); ?> 
+   
+
+      <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+	  <section class="hero-wrap"  style="background-image: url('images/bg_3.jpg');" data-section="home" data-stellar-background-ratio="0.5" data-spy="scroll" data-target=".site-navbar-target" data-offset="200">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
+          <div class="col-md-6 pt-5 ftco-animate">
+          </div>
+        </div>
+      </div>
+    </section>
+    <h1 class="department_header text-center mt-5 pt-5 ">Department</h1>
+
+		<section class="ftco-section ftco-no-pt ftco-no-pb mt-20" id="department-section">
+    	<div class="container-fluid px-0">
+    		<div class="row no-gutters">
+    			<div class="col-md-4 d-flex">
+    				<div class="img img-dept align-self-stretch" style="background-image: url(images/dept-1.jpg);"></div>
+    			</div>
+    			<div class="col-md-8">
+    				<div class="row no-gutters">
+    					<div class="col-md-4">
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Neurology</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Surgical</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Dental</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    					</div>
+    					<div class="col-md-4">
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Opthalmology</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Cardiology</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Traumatology</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    					</div>
+    					<div class="col-md-4">
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Nuclear Magnetic</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">X-ray</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Cardiology</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+    <section class="ftco-section" id="doctor.php">
+			<div class="container-fluid px-5">
+				<div class="row justify-content-center mb-5 pb-2">
+          <div class="col-md-8 text-center heading-section ftco-animate">
+            <h2 class="mb-4">Our Qualified Doctors</h2>
+            <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+          </div>
+        </div>	
+				<div class="row">
+					<div class="col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch">
+								<div class="img align-self-stretch" style="background-image: url(images/doc-1.jpg);"></div>
+							</div>
+							<div class="text pt-3 text-center">
+								<h3 class="mb-2">Dr. Lloyd Wilson</h3>
+								<span class="position mb-2">Neurologist</span>
+								<div class="faded">
+									<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+									<ul class="ftco-social text-center">
+		                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		              <p><a href="#" class="btn btn-primary">Book now</a></p>
+	              </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch">
+								<div class="img align-self-stretch" style="background-image: url(images/doc-2.jpg);"></div>
+							</div>
+							<div class="text pt-3 text-center">
+								<h3 class="mb-2">Dr. Rachel Parker</h3>
+								<span class="position mb-2">Ophthalmologist</span>
+								<div class="faded">
+									<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+									<ul class="ftco-social text-center">
+		                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		              <p><a href="#" class="btn btn-primary">Book now</a></p>
+	              </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch">
+								<div class="img align-self-stretch" style="background-image: url(images/doc-3.jpg);"></div>
+							</div>
+							<div class="text pt-3 text-center">
+								<h3 class="mb-2">Dr. Ian Smith</h3>
+								<span class="position mb-2">Dentist</span>
+								<div class="faded">
+									<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+									<ul class="ftco-social text-center">
+		                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		              <p><a href="#" class="btn btn-primary">Book now</a></p>
+	              </div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 col-lg-3 ftco-animate">
+						<div class="staff">
+							<div class="img-wrap d-flex align-items-stretch">
+								<div class="img align-self-stretch" style="background-image: url(images/doc-4.jpg);"></div>
+							</div>
+							<div class="text pt-3 text-center">
+								<h3 class="mb-2">Dr. Alicia Henderson</h3>
+								<span class="position mb-2">Pediatrician</span>
+								<div class="faded">
+									<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+									<ul class="ftco-social text-center">
+		                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
+		                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+		              </ul>
+		              <p><a href="#" class="btn btn-primary">Book now</a></p>
+	              </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="ftco-facts img ftco-counter" style="background-image: url(images/bg_3.jpg);">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row d-flex align-items-center">
+					<div class="col-md-5 heading-section heading-section-white">
+						<span class="subheading">Fun facts</span>
+						<h2 class="mb-4">Over 5,100 patients trust us</h2>
+						<p class="mb-0"><a href="#" class="btn btn-secondary px-4 py-3">Make an appointment</a></p>
+					</div>
+					<div class="col-md-7">
+						<div class="row pt-4">
+		          <div class="col-md-6 d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18">
+		              <div class="text">
+		                <strong class="number" data-number="30">0</strong>
+		                <span>Years of Experienced</span>
+		              </div>
+		            </div>
+		          </div>
+		          <div class="col-md-6 d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18">
+		              <div class="text">
+		                <strong class="number" data-number="4500">0</strong>
+		                <span>Happy Patients</span>
+		              </div>
+		            </div>
+		          </div>
+		          <div class="col-md-6 d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18">
+		              <div class="text">
+		                <strong class="number" data-number="84">0</strong>
+		                <span>Number of Doctors</span>
+		              </div>
+		            </div>
+		          </div>
+		          <div class="col-md-6 d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18">
+		              <div class="text">
+		                <strong class="number" data-number="300">0</strong>
+		                <span>Number of Staffs</span>
+		              </div>
+		            </div>
+		          </div>
+	          </div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+
+    
+ 
+    
+  </body>
+  
+
+
+  <!-- loader -->
+  
+  <?php require_once "inc/footer.php"; ?>
+<?php require_once "inc/jsScript.php"; ?>
