d284e6a2e8bdea78157b2558f0de5be4a14243c5
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-08-10 07:04:21 +0430

    adding files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    window.setTimeout(function () {
+        $(".alert").slideUp(500);
+    }, 5000);
+
+
+    $(".delete").click(function () {
+        var result = window.confirm("Are you sure want to delete this row");
+        if (!result) {
+            Event.preventDefault();
+        }
+    });
+
+});
