5c21878e1d4e522bb62d64fe73484ead02118e1e
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-08-10 07:00:24 +0430

    create department and contact page

---
+++ department.php
@@ -0,0 +1,111 @@
+		
+		  <?php require_once "inc/head.php"; ?>
+		 <?php include_once "inc/navbar.php"; ?>
+		 <h1 class="department_header text-center mt-5 pt-5 ">Department</h1>
+
+		<section class="ftco-section ftco-no-pt ftco-no-pb mt-20" id="department-section">
+    	<div class="container-fluid px-0">
+    		<div class="row no-gutters">
+    			<div class="col-md-4 d-flex">
+    				<div class="img img-dept align-self-stretch" style="background-image: url(images/dept-1.jpg);"></div>
+    			</div>
+
+    			<div class="col-md-8">
+    				<div class="row no-gutters">
+    					<div class="col-md-4">
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Neurology</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Surgical</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Dental</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    					</div>
+
+    					<div class="col-md-4">
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Opthalmology</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Cardiology</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Traumatology</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    					</div>
+
+    					<div class="col-md-4">
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Nuclear Magnetic</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">X-ray</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    						<div class="department-wrap p-4 ftco-animate">
+    							<div class="text p-2 text-center">
+    								<div class="icon">
+    									<span class="flaticon-stethoscope"></span>
+    								</div>
+    								<h3><a href="#">Cardiology</a></h3>
+    								<p>Far far away, behind the word mountains</p>
+    							</div>
+    						</div>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+
+	<?php require_once "inc/footer.php"; ?>
+	<?php require_once "inc/jsScript.php"; ?>
